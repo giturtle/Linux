71d4fd5992d5b45ad0fbaa7fc47421d436438684
giturtle <44741844+giturtle@users.noreply.github.com>
2019-03-27 00:09:00 +0800

    等待进程

---
+++ wait.c
@@ -0,0 +1,19 @@
+#include <unistd.h>    
+#include <stdlib.h>    
+    
+int main(){    
+  int status;
+  wait(&status);
+  int pid = fork();    
+  if(pid < 0){    
+    perror("fork eror");    
+  }    
+  else if(pid == 0){    
+    exit(0);    
+    sleep(1);    
+  }    
+  while(waitpid(pid,&status,0) == 0){
+		perror("waitpid error");
+	}
+  return 0;    
+}    
