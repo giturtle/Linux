dd88a5fe8203e1ef3e0c50571695d2be1c9590cf
giturtle <44741844+giturtle@users.noreply.github.com>
2019-05-17 23:22:55 +0800

    Socket UDP Server

---
+++ Socket/UDP_server.cpp
@@ -0,0 +1,33 @@
+/*=============================================================== 
+*   描    述：通过UdpSocket实现udp服务端程序 
+================================================================*/
+#include "udpsocket.hpp"
+
+#define CHECK_RET(q) if((q) == false){return -1;}
+
+int main(int argc, char *argv[]){
+    if (argc != 3) {
+        printf("./udp_srv ip port\n");
+        return -1;
+    }
+    std::string ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    UdpSocket sock;
+    CHECK_RET(sock.Socket());
+    CHECK_RET(sock.Bind(ip, port));
+
+    while(1) {
+        std::string buf;
+        struct sockaddr_in cli_addr;
+        CHECK_RET(sock.Recv(buf, &cli_addr));
+        std::cout<<"client say:"<<buf<<std::endl;
+        
+        std::cout<<"server say:";
+        fflush(stdout);
+        std::cin>> buf;
+        CHECK_RET(sock.Send(buf, &cli_addr));
+    }
+    sock.Close();
+}
+
